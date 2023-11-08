4d5dae6272e2731d8dac533389239a733d8de90a
AleksandrVoropaev <aleksandr.voropayev@gmail.com>
2023-11-08 20:41:08 +0100

    Initial Commit

---
+++ GuessTranslation/GuessTranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTranslationApp.swift
+//  GuessTranslation
+//
+//  Created by Oleksandr Voropayev on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTranslationUITests/GuessTranslationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GuessTranslationUITestsLaunchTests.swift
+//  GuessTranslationUITests
+//
+//  Created by Oleksandr Voropayev on 08.11.2023.
+//
+
+import XCTest
+
+final class GuessTranslationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
